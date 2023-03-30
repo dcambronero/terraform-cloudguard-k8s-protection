027e742beb2e232847a55cc18226f837c986bac2
dcambronero <89933914+dcambronero@users.noreply.github.com>
2023-03-30 11:05:25 -0600

    updated the hashicorp/kubernetes version

--- providers.tf
+++ providers.tf
@@ -11,7 +11,7 @@ terraform {
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~> 2.11.2"
+      version = ">= 2.0.1"
     }
     helm = {
       source = "hashicorp/helm"
